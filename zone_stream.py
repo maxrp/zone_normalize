73d3a1fdf8fcfeba5e1102fc5de22d09d1cad1da
Max Parmer <maxp@trystero.is>
2016-05-11 05:08:08 +0000

    Fleshes out parsing a bit more.

--- zone_stream.py
+++ zone_stream.py
@@ -3,21 +3,20 @@
 import gzip
 import sys
 
-from collections import defaultdict
+RECORDTYPES = ['a', 'aaaa', 'ns', 'rrsig', 'nsec', 'nsec3', 'nsec3param', 'dnskey']
 
-def strip_comments(line):
+def split_comments(line):
     semicolon = line.index(';')
-    return line[0:semicolon].strip()
+    return (line[0:semicolon].strip(), line[semicolon:].strip())
 
 def parse_zone(zone_file):
-    dispatch = defaultdict(None, {})
+    default_values = {}
+    multiline = False
+    multiline_str = ''
     with gzip.open(zone_file, mode='rt') as zonefh:
-        multiline = False
-        multiline_str = ''
         for line in zonefh:
             if ';' in line:
-                line = strip_comments(line)
-
+                line, _ = split_comments(line)
             if '(' in line:
                 multiline = True
 
@@ -26,12 +25,38 @@ def parse_zone(zone_file):
                 if ')' in multiline_str:
                     line = multiline_str
                     multiline = False
+                    multiline_str = ''
                 else:
                     continue
-          
+
             line = line.strip().split()
-            if line:
-                yield line
+            line_len = len(line)
+
+            # midpoint filtering
+            if line_len < 2:
+                continue
+
+            # set a new default value
+            if line_len == 2 and line[0].startswith('$'):
+                default_values[line[0][1:].lower()] = line[1]
+                continue
+
+            # replace @ with origin
+            if line[0] == '@':
+                line[0] = default_values['origin']
+
+            # Fixing phase
+            if line[1].lower() in RECORDTYPES:
+                # is the second field a known record type? then inject a TTL
+                line.insert(1, default_values['ttl'])
+            elif line[0].lower() in RECORDTYPES:
+                line.insert(0, default_values['ttl'])
+                line.insert(0, default_values['origin'])
+
+            if not line[0].endswith('.'):
+                line[0] += '.' + default_values['origin']
+
+            yield line
 
 def main():
     zone_file = sys.argv[1]
