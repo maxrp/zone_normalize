1182ea7397566769079112a78ef60f912d621339
Max Parmer <maxp@trystero.is>
2016-05-22 17:53:49 -0700

    Clarifies code, centralizes .lower() calls, increases comments.

--- src/zone_iterator/__init__.py
+++ src/zone_iterator/__init__.py
@@ -22,7 +22,7 @@ def split_comments(line: str) -> Tuple[str, str]:
 def set_defaults(line: str, default_values: Dict[str, str]) -> Dict[str, str]:
     var_name, var_value = tuple(line[1:].split())
     var_name = var_name.lower()
-    default_values[var_name] = var_value
+    default_values[var_name] = var_value.lower()
     return default_values
 
 
@@ -68,7 +68,7 @@ def zone_iterator(zone_file: Iterable,
         line_chunks = line.strip().split()
         line_len = len(line_chunks)
 
-        # midpoint filtering
+        # We need this line to at least be two fields, type and data
         if line_len < 2:
             continue
 
@@ -76,23 +76,32 @@ def zone_iterator(zone_file: Iterable,
         if line_chunks[0] == '@':
             line_chunks[0] = default_values['origin']
 
-        # Fixing phase
-        if line_chunks[1].lower() in RECORDCLASSES and \
-                line_chunks[2].lower() in RECORDTYPES:
-                    # then the first field is the ttl
-                    line_chunks.insert(0, default_values['origin'])
+        # always lowercase the first field
+        line_chunks[0] = line_chunks[0].lower()
 
-        if line_chunks[1].lower() in RECORDCLASSES and \
-                not line_chunks[0].isnumeric():
-                line_chunks.insert(1, default_values['ttl'])
-        elif line_chunks[1].lower() in RECORDTYPES:
-            # is the second field a known record type? then inject a TTL
-            line_chunks.insert(1, default_values['class'])
-            line_chunks.insert(1, default_values['ttl'])
-        elif line_chunks[0].lower() in RECORDTYPES:
+        # A two field [type, data] entry, needs three fields added
+        if line_chunks[0] in RECORDTYPES and line_len == 2:
             line_chunks.insert(0, default_values['class'])
             line_chunks.insert(0, default_values['ttl'])
             line_chunks.insert(0, default_values['origin'])
+
+        # Normalize case in the first three fields for ease of comparison
+        for i in range(1, 3):
+            line_chunks[i] = line_chunks[i].lower()
+
+        if line_chunks[1] in RECORDCLASSES and line_chunks[2] in RECORDTYPES:
+            # then the first field is the ttl
+            line_chunks.insert(0, default_values['origin'])
+
+        # if the first char of the first field isn't numeric and the second is
+        # a class, inject a ttl field
+        if line_chunks[1] in RECORDCLASSES \
+                and not line_chunks[0][0].isnumeric():
+            line_chunks.insert(1, default_values['ttl'])
+        # if the second field is a known record type inject a ttl and class
+        elif line_chunks[1] in RECORDTYPES:
+            line_chunks.insert(1, default_values['class'])
+            line_chunks.insert(1, default_values['ttl'])
 
         if not line_chunks[0].endswith('.'):
             line_chunks[0] += '.' + default_values['origin']
