dfa9655ce8a6e412aaa12d0023c10b4e7261360e
Max Parmer <maxp@trystero.is>
2016-05-23 21:42:46 -0700

    Lint on setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+import os
+import sys
+
 from setuptools import setup, find_packages
-import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
@@ -12,23 +14,23 @@ if sys.version_info < (3, 5, 0):
     install_requires.append('typing>=3.5')
 
 setup(name='zone_iterator',
-    version=version,
-    description="",
-    long_description=README + '\n\n' + NEWS,
-    classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='',
-    author='Max R.D. Parmer',
-    author_email='maxp@trystero.is',
-    url='',
-    license='AGPLv3',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
-    install_requires=install_requires,
-    setup_requires=['pytest-runner'],
-    entry_points={
-        'console_scripts':
-            ['zone_normalize=zone_iterator.__main__:main']
-    }
-)
+      version=version,
+      description="",
+      long_description=README + '\n\n' + NEWS,
+      classifiers=[
+        # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      ],
+      keywords='',
+      author='Max R.D. Parmer',
+      author_email='maxp@trystero.is',
+      url='',
+      license='AGPLv3',
+      packages=find_packages('src'),
+      package_dir={'': 'src'}, include_package_data=True,
+      install_requires=install_requires,
+      setup_requires=['pytest-runner'],
+      entry_points={
+        'console_scripts':  [
+            'zone_normalize=zone_iterator.__main__:main'
+        ]
+      })
