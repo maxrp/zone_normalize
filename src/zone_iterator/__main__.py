864f3ca7c1e9395b517b39a9344bd9cf6f86a720
Max Parmer <maxp@trystero.is>
2016-05-29 21:00:16 -0700

    Adds dump mode.

--- src/zone_iterator/__main__.py
+++ src/zone_iterator/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import gzip
 
+from pprint import PrettyPrinter
 
 try:
     # colorama is an optional dep
@@ -24,7 +25,16 @@ def maybe_compressed_file(filename):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('inputs', nargs='*', type=maybe_compressed_file)
+    parser.add_argument('-d',
+                        '--dump',
+                        action='store_true',
+                        help='Dump a list of line structures instead of \
+                        printing and coloring each entry.')
+    parser.add_argument('zones',
+                        nargs='*',
+                        type=maybe_compressed_file,
+                        help='A file or list of zone files, optionally these \
+                        files may be gzipped.')
     args = parser.parse_args()
 
     if HAS_COLOR:
@@ -36,10 +46,14 @@ def main():
                                  zip(colors, unpacked_fmt)
                                  for color in segment])
 
-    for inputfile in args.inputs:
-        with inputfile as zonefh:
-            for record in zone_iterator(zonefh):
-                print(zone_dict_to_str(record, fmt_str=color_format))
+    for zone in args.zones:
+        with zone as zonefh:
+            if args.dump:
+                pp = PrettyPrinter(indent=4, compact=True, width=72)
+                pp.pprint([l for l in zone_iterator(zonefh)][0:9])
+            else:
+                for record in zone_iterator(zonefh):
+                    print(zone_dict_to_str(record, fmt_str=color_format))
 
 
 if __name__ == "__main__":
